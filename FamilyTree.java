ff01e3aec723e944ebe444a744a0fdd9e577ac96
Sergey <151991545+SergeyBilyamov@users.noreply.github.com>
2024-06-15 08:03:24 +0500

    Add files via upload

---
+++ FamilyTree.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private List<Person> members;
+
+    public FamilyTree() {
+        this.members = new ArrayList<>();
+    }
+
+    public void addMember(Person person) {
+        if (person != null) {
+            this.members.add(person);
+        } else {
+            throw new IllegalArgumentException("Member cannot be null");
+        }
+    }
+
+    public Person findPersonByName(String name) {
+        for (Person person : members) {
+            if (person.getName().equals(name)) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public List<Person> getAllChildren(String personName) {
+        Person person = findPersonByName(personName);
+        if (person != null) {
+            return person.getChildren();
+        } else {
+            return null;
+        }
+    }
+}
